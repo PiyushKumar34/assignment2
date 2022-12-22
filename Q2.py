f7781e3814ce685ce447abe3585d4ee762f06cc3
PiyushKumar34 <121247590+PiyushKumar34@users.noreply.github.com>
2022-12-22 23:49:32 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,5 +1,5 @@
-name="Hitesh Kochar"
-sid=22104074
-dept="Electrical"
-cg=10.0
+name="Piyush Kumar"
+sid=20107105
+dept="mechanical"
+cg=10
 print(" Hey,",name,"Here!\n","My SID is",sid,"\n","I am from",dept,"Department and my CGPA is",cg)
